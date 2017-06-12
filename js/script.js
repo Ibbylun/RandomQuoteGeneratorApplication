85381149f7a35f787da5ab8940961c43058fe084
Ibbylun <Ibrahimcetinkaya@live.com.au>
2017-06-12 16:32:16 +1000

    Made the Button Listener
    Made the button listener that listens for a click --> generates a random number ---> grabs the html by the id --> (Doesn't insert into the box though).

--- js/script.js
+++ js/script.js
@@ -1,16 +1,10 @@
-// event listener to respond to "Show another quote" button clicks
+document.getElementById("loadQuote").addEventListener("click", getRandomQuote);
 
-/*
+function getRandomQuote(){
+    var generateRandomNumber = Math.floor(Math.random * quoteArray.length);
+    document.getElementById("quote").innerHTML = quoteArray[generateRandomNumber];
+}
 
-button.addEventListener('click',);
-
-*/
-
-
-// when user clicks anywhere on the button, the "printQuote" function is called
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
-//object of quotes, sources, citations and years
 var quoteArray = [{
     quote:'The best preparation for tomorrow is doing your best today.',source:'H. Jackson Brown, Jr.',citation:'',year:''
 }, {
@@ -20,22 +14,6 @@ var quoteArray = [{
 }, {
     quote:'The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.',source:'Hellen Keller',citation:'',year:''
 }, {
-    quote:'Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. And the only way to do great work is to love what you do. If you haven't found it yet, keep looking. Don't settle. As with all matters of the heart, you\'ll know when you find it.',source:'Steve Jobs',citation:'',year:''
-}];
-
-//Grabbing HTML Elements
-var button = document.getElementById('loadQuote'),
-    quote = document.getElementById('quote'),
-    author = document.getElementById('source');
-
-//Random Quote Function
-var getRandomQuote{
-    var generateRandomNumber = Math.floor(Math.random * quoteArray.length);
-    var getRandomText = quoteArray[generateRandomNumber].quote;
-    var getRandomSource = quoteArray[generateRandomNumber].source;
-    var getRandomCitation = quoteArray[generateRandomNumber].citation;
-    var getRandomYear = quoteArray[generateRandomNumber].year;
-}
-
+    quote:'Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. And the only way to do great work is to love what you do. If you haven\'t found it yet, keep looking. Don\'t settle. As with all matters of the heart, you\'ll know when you find it.',source:'Steve Jobs',citation:'',year:''}];
 
 
